7768f8f70af679c38deb405ac96a71b26375131d
glottsi <glottsifromtwitch@gmail.com>
2020-08-09 20:10:16 -0400

    with env variables database

--- backend/mysql.js
+++ backend/mysql.js
@@ -82,7 +82,7 @@ const initializeDatabase = (callback) => {
 
   // Post gresl
 
-  const connectionString = process.env.DB_URL;
+  const connectionString = process.env.DATABASE_URL;
 
   const client = new Client({
     connectionString: connectionString,
