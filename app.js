d8ed50a44939214b1bdafdfeef9f639c9da8f617
Paul Nguyen <paul@trustprovenance.com>
2021-01-20 03:53:31 +1030

    Error Handling

--- app.js
+++ app.js
@@ -19,4 +19,18 @@ app.set("view engine", "ejs");
 // routes
 app.use("/", indexRouter);
 
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error("Not found");
+  err.status(404);
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
 module.exports = app;
