ce89b0b6863d924277abc84d4b004a45ca1e387d
Paul Nguyen <paul@trustprovenance.com>
2021-01-20 03:03:18 +1030

    Clean up app.js, use ExpressJS code structure

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var path = require("path");
 var logger = require("morgan");
-var debug = require("debug")("streamer-matcher:server");
-var http = require("http");
 
 const calculateStreamer = require("./backend/calculate_streamer");
 const createStreamer = require("./backend/create_streamer");
@@ -76,58 +74,4 @@ app.get("*", (req, res) => {
   res.redirect("/404");
 });
 
-/**
- * Get port from environment and store in Express.
- */
-
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-
-var port = process.env.PORT || 3000;
-app.set("port", port);
-
-/**
- * Create HTTP server and listen on provided port, on all network interfaces.
- */
-
-var server = http.createServer(app);
-server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-
-  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-  debug("Listening on " + bind);
-}
+module.exports = app;
