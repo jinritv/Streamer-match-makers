709e2b7a2c6207fc35e8c0cd47f4ff828bc7eba4
junglemoon82 <newpath7@gmail.com>
2021-02-05 06:09:15 +0000

    Theme & language presistence via express-session -junglemoon

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var path = require("path");
 var logger = require("morgan");
+var session = require("express-session");
 
 var indexRouter = require("./routes/index");
 
@@ -11,6 +12,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session({secret: 'jin.ri.tv'}));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
