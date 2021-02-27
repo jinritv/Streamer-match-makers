ded87ca4846e0bfc6a20e313148ab14ee3d907eb
Ron San Jose <ronsj1@gmail.com>
2021-02-26 22:48:49 -0800

    Cleanup ProgressBar

--- pages/components/ProgressBar.js
+++ pages/components/ProgressBar.js
@@ -1,27 +1,17 @@
-import { useEffect } from 'react'
-
 export default function ProgressBar(props) {
   const segments = new Array(props.quizLength)
     .fill(null)
     .map((s, index) => index)
 
-  useEffect(() => {
-    console.log(props.currentQuestion)
-  }, [props.currentQuestion])
-
   return (
     <div className="quiz-progress-bar">
       {segments.map((i) => {
-        return (
-          <span
-            key={i}
-            className={
-              props.currentQuestion > i
-                ? 'quiz-progress-bar-segment-complete'
-                : 'quiz-progress-bar-segment'
-            }
-          />
-        )
+        const segmentClassName =
+          props.currentQuestion > i
+            ? 'quiz-progress-bar-segment-complete'
+            : 'quiz-progress-bar-segment'
+
+        return <span key={i} className={segmentClassName} />
       })}
     </div>
   )
