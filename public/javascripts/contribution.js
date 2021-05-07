7ae3dd03216a7a084444cc31383627a92c85e28b
Seyther <Seyther@users.noreply.github.com>
2021-05-07 18:16:57 +0800

    Dark Mode Activated
    - Added dark mode to contribution page, submission page, about page
    - Fixed colors according to Figma's dark mode design

--- public/javascripts/contribution.js
+++ public/javascripts/contribution.js
@@ -18,7 +18,32 @@ const carouselSlideList = [
 
 $(document).ready(function () {
   setCarouselSlide(0);
+  setLanguage(getDefaultLanguage());
+  assignThemes();
+  getDataForTheme();
 });
+
+// loads the rest of the data in JSON form (not rendered html)
+function getDataForTheme() {
+  $.ajax({
+    beforeSend: console.log("getting rest of data"),
+    url: "/getQuizData",
+    type: "POST",
+    data: { language: getLanguage() },
+    success: (data) => {
+      if (data.err) {
+        console.error("Error getting data!");
+      } else {
+        // data is loaded so now start the page effects
+        setLanguageTexts(data);
+      }
+    },
+  });
+}
+
+function setLanguageTexts(extraData) {
+  setTexts(extraData.requiredTexts);
+}
 
 $("#carouselIndicators").on("slide.bs.carousel", function (event) {
   setCarouselSlide(event.to);
