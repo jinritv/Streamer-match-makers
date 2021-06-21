fe3e60d6cb9c2b6233ebb116727842c5b32df7ef
Seyther <Seyther@users.noreply.github.com>
2021-06-21 23:03:21 +0800

    Mobile Update
    - Updated css and media queries for mobile UI, should look better now on iPhone 5 / SE / 6 / 6 Plus / 7 / 7 Plus / 8 / 8 Plus / X

--- backend/quiz_questions.js
+++ backend/quiz_questions.js
@@ -10,14 +10,14 @@ we can allow more than 1 button active at a given time.
 
  */
 const QuestionTypes = {
-    Buttons: {
-      MultipleSelection: "multipleselection",
-      SingleSelection: "singleselection",
-    },
-    RangeSlider: "rangeslider",
-    TimeRange: "timerange",
-  };
-  
+  Buttons: {
+    MultipleSelection: "multipleselection",
+    SingleSelection: "singleselection",
+  },
+  RangeSlider: "rangeslider",
+  TimeRange: "timerange",
+};
+
 // Specific settings for each type of question
 const QuestionTypeSettings = {
   [QuestionTypes.Buttons.MultipleSelection]: {
@@ -35,59 +35,58 @@ const QuestionTypeSettings = {
     // no default settings for this type of question
   },
 };
-  
-  // the quiz's questions will be built and displayed in the order they appear in this array
-  const QuizQuestions = [
-    // Question 1: 'languages'
-    {
-      unique_question_identifier: "languages", //unique name for this question
-      question_type: QuestionTypes.Buttons.MultipleSelection, // type of question as explained above
-      disableContinueButtonByDefault: true, // should the 'Continue' button get disabled when the question is displayed (is the it instantly skippable?)
-      buttonsPerRow: 3, // if you include this property, you override the default buttons per row
-      onclickFunctionName: "exampleOnClickFunction", // you can override the function that is called when the button is pressed
-      answer_settings: [
-        // list values for the answers here
-        "english",
-        "korean",
-        "japanese",
-        "chinese",
-        "french",
-        "spanish",
-      ],
-    },
-  
-    // Question 2: 'content'
-    {
-      unique_question_identifier: "content",
-      question_type: QuestionTypes.Buttons.MultipleSelection,
-      disableContinueButtonByDefault: true,
-      answer_settings: [
 
-        "justchatting",
-        "games",
-        "sciencetech",
-        "music",
-        "art",
-        "food",
-        "talk",
-        "maker",
-        "travel",
-        "asmr",
-        "fitness",
-        "events",
-        "sports",
-        "politics",
-        "quiz",
-        "crypto",
-        "stock",
-        "casino",
-        "board",
-        "bodyart",
-        "tarot",
-        "makeup",
-        "sleep",
-        "twitch",
-        /*"justchatting",
+// the quiz's questions will be built and displayed in the order they appear in this array
+const QuizQuestions = [
+  // Question 1: 'languages'
+  {
+    unique_question_identifier: "languages", //unique name for this question
+    question_type: QuestionTypes.Buttons.MultipleSelection, // type of question as explained above
+    disableContinueButtonByDefault: true, // should the 'Continue' button get disabled when the question is displayed (is the it instantly skippable?)
+    buttonsPerRow: 3, // if you include this property, you override the default buttons per row
+    onclickFunctionName: "exampleOnClickFunction", // you can override the function that is called when the button is pressed
+    answer_settings: [
+      // list values for the answers here
+      "english",
+      "korean",
+      "japanese",
+      "chinese",
+      "french",
+      "spanish",
+    ],
+  },
+
+  // Question 2: 'content'
+  {
+    unique_question_identifier: "content",
+    question_type: QuestionTypes.Buttons.MultipleSelection,
+    disableContinueButtonByDefault: true,
+    answer_settings: [
+      "justchatting",
+      "games",
+      "sciencetech",
+      "music",
+      "art",
+      "food",
+      "talk",
+      "maker",
+      "travel",
+      "asmr",
+      "fitness",
+      "events",
+      "sports",
+      "politics",
+      "quiz",
+      "crypto",
+      "stock",
+      "casino",
+      "board",
+      "bodyart",
+      "tarot",
+      "makeup",
+      "sleep",
+      "twitch",
+      /*"justchatting",
         "games",
         "ASMR",
         "sciencetech",
@@ -99,83 +98,85 @@ const QuestionTypeSettings = {
         "music",
         "dancing",
         "yoga",*/
-      ],
-    },
-  
-    //Question 3: Follower/Sub only chat?
-    {
-      unique_question_identifier: "subonly",
-      question_type: QuestionTypes.Buttons.SingleSelection,
-      disableContinueButtonByDefault: true,
-      answer_settings: ["all", "sub-only", "follower-only"],
-    },
-  
-    //Question 4: Mature
-    {
-      unique_question_identifier: "mature",
-      question_type: QuestionTypes.Buttons.SingleSelection,
-      disableContinueButtonByDefault: true,
-      answer_settings: [true, false],
-    },
-  
-    // Question 5: chat vibes
-    {
-      unique_question_identifier: "chat_vibe",
-      question_type: QuestionTypes.Buttons.MultipleSelection,
-      disableContinueButtonByDefault: true,
-      buttonsPerRow: 5,
-      answer_settings: [
-        "chatty",
-        "chill",
-        "serious",
-        "smart",
-        "funny",
-        "friendly",
-        "moody",
-        "weird",
-        "geeky",
-        "shy",
-        "silly",
-        "rude",
-        "dorky",
-        "angry",
-        "loud",
-        "quiet",
-        "troll",
-        "drunk",
-        "spam-emotes",
-        "fast",
-        "slow",
-        "wholesome",
-        "toxic",
-      ],
-    },
-  
-    // Question 6: average_viewers
-    {
-      unique_question_identifier: "average_viewers",
-      question_type: QuestionTypes.RangeSlider,
-      disableContinueButtonByDefault: false,
-      answer_settings: {
-        min: 0,
-        max: 2000,
-        incrementBy: 50,
-        defaultMin: 200,
-        defaultMax: 1000,
-      },
-    },
-  
-    // Question 7: 'gender'
-      {
-        unique_question_identifier: "gender",
-        question_type: QuestionTypes.Buttons.SingleSelection,
-        disableContinueButtonByDefault: true,
-        answer_settings: [
-          "male",
-          "female",
-          "nopreference",
-        ],
-      },
-  ];
+    ],
+  },
 
-  module.exports = {Quiz: {Types:QuestionTypes,Questions:QuizQuestions,Settings:QuestionTypeSettings}};
+  //Question 3: Follower/Sub only chat?
+  {
+    unique_question_identifier: "subonly",
+    question_type: QuestionTypes.Buttons.SingleSelection,
+    disableContinueButtonByDefault: true,
+    answer_settings: ["all", "sub-only", "follower-only"],
+  },
+
+  //Question 4: Mature
+  {
+    unique_question_identifier: "mature",
+    question_type: QuestionTypes.Buttons.SingleSelection,
+    disableContinueButtonByDefault: true,
+    answer_settings: [true, false],
+  },
+
+  // Question 5: chat vibes
+  {
+    unique_question_identifier: "chat_vibe",
+    question_type: QuestionTypes.Buttons.MultipleSelection,
+    disableContinueButtonByDefault: true,
+    buttonsPerRow: 4,
+    answer_settings: [
+      "chatty",
+      "chill",
+      "serious",
+      "smart",
+      "funny",
+      "friendly",
+      "moody",
+      "weird",
+      "geeky",
+      "shy",
+      "silly",
+      "rude",
+      "dorky",
+      "angry",
+      "loud",
+      "quiet",
+      "troll",
+      "drunk",
+      "spam-emotes",
+      "fast",
+      "slow",
+      "wholesome",
+      "toxic",
+    ],
+  },
+
+  // Question 6: average_viewers
+  {
+    unique_question_identifier: "average_viewers",
+    question_type: QuestionTypes.RangeSlider,
+    disableContinueButtonByDefault: false,
+    answer_settings: {
+      min: 0,
+      max: 2000,
+      incrementBy: 50,
+      defaultMin: 200,
+      defaultMax: 1000,
+    },
+  },
+
+  // Question 7: 'gender'
+  {
+    unique_question_identifier: "gender",
+    question_type: QuestionTypes.Buttons.SingleSelection,
+    disableContinueButtonByDefault: true,
+    answer_settings: ["male", "female", "nopreference"],
+  },
+];
+
+module.exports = {
+  Quiz: {
+    Types: QuestionTypes,
+    Questions: QuizQuestions,
+    Settings: QuestionTypeSettings,
+  },
+};
